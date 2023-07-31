0be2f07c565b76d632095732fcdbfeab7ebbcee7
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-31 17:12:57 -0400

    Add array index js

--- index.js
+++ index.js
@@ -4,7 +4,63 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+questions = [
+
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+
+    {
+        type: "input",
+        message: "What is a short description of your project?",
+        name: "description"
+    },
+
+    {
+        type: "input",
+        message: "Please provide instructions for installing your project.",
+        name: "instructions"
+    },
+
+    {
+        type: "input",
+        message: "Please enter any usage information.",
+        name: "usage"
+    },
+
+    {
+        type: "list",
+        message: "Please select a license from the following:",
+        name: "license",
+        choices:['MIT', 'Apache', 'NONE']
+    },
+
+    {
+        type: "input",
+        message: "Please add your contribution guidelines.",
+        name: "contribution"
+    },
+
+    {
+        type: "input",
+        message: "Please enter any test instructions.",
+        name: "test"
+    },
+
+    {
+        type: "input",
+        message: "Please enter your GitHub username.",
+        name: "username"
+    },
+
+    {
+        type: "input",
+        message: "Please enter your email address.",
+        name: "email"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
