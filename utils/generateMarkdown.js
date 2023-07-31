58555d0344ad5be2ced0c13a3cf53107646fbf78
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-31 17:17:38 -0400

    Add renderLicenseBadge gen mark js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,1 +1,20 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+      return `![${license}](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    } else if (license === "Apache") {
+      return `![${license}](https://img.shields.io/badge/license-${license}-green.svg)`;
+    } else if (license === "NONE") {
+      return ` `;
+    }
+  }
 
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
